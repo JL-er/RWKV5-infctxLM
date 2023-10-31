4a0aac4c677f877f0bc752f7cfe55723fd6aa8a3
JL-er <997529190@qq.com>
2023-10-31 10:33:10 +0800

    my_save

--- src/trainer.py
+++ src/trainer.py
@@ -4,23 +4,14 @@ from torch.utils.data import DataLoader
 import pytorch_lightning as pl
 from pytorch_lightning.utilities import rank_zero_info, rank_zero_only
 
-def my_save(args, trainer, dd, ff):
-    if '14b-run1' in ff:
+def my_save(dd, ff):
+    if '14b-run1' not in ff:
+        torch.save(dd, ff)
+    else:
         fn = ff.split('/')[-1]
         fff = '/dev/shm/' + fn
         torch.save(dd, fff)
         subprocess.Popen(f" aws s3 mv {fff} s3://rwkv-14b-4k/{fn} --quiet", shell=True)
-    elif ('world/14b' in ff) or ('world/7b' in ff):
-        aa = ff.split('/')[1]
-        fn = ff.split('/')[-1]
-        fff = f'/dev/shm/{aa}-{fn}'
-        torch.save(dd, fff)
-        subprocess.Popen(f" aws s3 mv {fff} s3://rwkv-world/{aa}-{fn} --quiet", shell=True)
-    else:
-        if 'deepspeed_stage_3' in args.strategy:
-            trainer.save_checkpoint(ff, weights_only=True)
-        else:
-            torch.save(dd, ff)
 
 class train_callback(pl.Callback):
     def __init__(self, args):
